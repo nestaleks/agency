852a91c5656df6f0ff45542f6364575f6d4c0acc
nestaleks <nestaleks83@gmail.com>
2024-03-26 15:48:42 +0200

    1

---
+++ js/main.js
@@ -0,0 +1,47 @@
+// мобільне меню
+const menuBtn = document.querySelector('.menu__btn');
+const menuList = document.querySelector('.menu__list');
+const menuClose = document.querySelector('.menu__close');
+
+menuBtn.addEventListener('click', ()=>{
+    menuList.classList.toggle('menu--open');
+});
+menuClose.addEventListener('click', ()=>{
+    menuList.classList.remove('menu--open');
+});
+
+//закриття бургера після кліку на посилання
+// $('.menu__btn').on('click', function () {
+// 	$('.menu__list').addClass('menu--open');
+//   });
+  
+//   $( '.menu__link' ).on("click", function(){
+// 	$('.menu__list').removeClass('menu--open');
+//   });
+
+
+
+//кнопка Наверх
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("upBtn").style.display = "block";
+    } else {
+        document.getElementById("upBtn").style.display = "none";
+    }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
+
+const icons = document.querySelectorAll('.menu__btn');
+icons.forEach (icon => {  
+  icon.addEventListener('click', (event) => {
+    icon.classList.toggle("open");
+  });
+});
