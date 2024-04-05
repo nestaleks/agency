d50be281cda84b458716fb1817dc284f4fc244d4
nestaleks <nestaleks83@gmail.com>
2024-04-05 12:00:46 +0300

    1

--- js/main.js
+++ js/main.js
@@ -45,3 +45,49 @@ icons.forEach (icon => {
     icon.classList.toggle("open");
   });
 });
+
+
+$('.services__items').slick({
+    dots: false,
+    infinite: true,
+    loop: true,
+    speed: 400,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      ,
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
